ca5ddfddd66d1d5bccf7cbdd9a66e8e50a9f0fd7
Adilson Fuxe <developer@adilsonfuxe.com>
2026-01-13 23:04:27 +0100

    feat: update to snake case

--- day-004/set-mismatch.rs
+++ day-004/set-mismatch.rs
@@ -7,8 +7,8 @@ fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
     let mut map = std::collections::HashMap::new();
     let mut wrong_number = -1;
     let mut sorted = nums.clone();
-    let mut existedValues = Vec::new();
-    let mut lastValue = -1;
+    let mut existed_values = Vec::new();
+    let mut last_value = -1;
     sorted.sort();
 
     //println!("{:?}", sorted);
@@ -23,23 +23,23 @@ fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
         if map.contains_key(&sorted[i as usize]) {
             result.push(sorted[i as usize]);
         } else {
-            existedValues.push(sorted[i as usize]);
+            existed_values.push(sorted[i as usize]);
             map.insert(sorted[i as usize], sorted[i as usize]);
         }
     }
 
    
-    for i in 0..existedValues.len() {
-        lastValue = existedValues[i as usize];
-        if existedValues[i as usize] != (i as i32) + 1 {
+    for i in 0..existed_values.len() {
+        last_value = existed_values[i as usize];
+        if existed_values[i as usize] != (i as i32) + 1 {
             wrong_number = (i as i32) + 1;
             result.push(wrong_number);
             return result;
         }
     }
 
-    if lastValue != (nums.len() as i32) {
-        result.push(lastValue + 1);
+    if last_value != (nums.len() as i32) {
+        result.push(last_value + 1);
     }
 
     result
